a8dd081541b4a768ea2f706b69e5d0585f5974e8
Mageas <gpg@mageas.net>
2023-01-25 17:14:15 +0100

    Update: remove copy/paste comments

--- src/model/get_account_payments.rs
+++ src/model/get_account_payments.rs
@@ -23,22 +23,6 @@ pub struct GetAccountPaymentsResponse {
     days: usize,
     #[serde(rename = "type")]
     _type: String,
-    // #[serde(deserialize_with = "deserialize_usize_to_bool")]
-    // pub premium: bool,
-    // pub login: String,
-    // pub email: String,
-    // pub point: String,
-    // pub premium_expire: String,
-    // #[serde(deserialize_with = "deserialize_usize_to_bool")]
-    // #[serde(rename = "securityLock")]
-    // pub security_lock: bool,
-    // #[serde(deserialize_with = "deserialize_usize_to_bool")]
-    // #[serde(rename = "directDownload")]
-    // pub direct_download: bool,
-    // #[serde(deserialize_with = "deserialize_usize_to_bool")]
-    // #[serde(rename = "sslDownload")]
-    // pub ssl_download: bool,
-    // pub token: String,
 }
 
 impl DeserializeCheck for GetAccountPaymentsResponseWrapper {
